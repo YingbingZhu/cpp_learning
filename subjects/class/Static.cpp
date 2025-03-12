4440a1ae3968299945e1e480be0fa5e770b23cda
zhuyingb <yingbing.zhu@hotmail.com>
2025-03-12 11:24:41 -0700

    static

--- subjects/class/Static.cpp
+++ subjects/class/Static.cpp
@@ -1,8 +1,21 @@
 //
 // Created by yingbingzhu on 2025/2/20.
 //
+#include <iostream>
+class Singleton {
+  public:
+    static Singleton& Get() {
+      static Singleton instance; // extend lifetime
+      return instance;
+    }
 
+  void Hello() {}
+};
 
 static int s_Variable = 10;
 
 int Variable = 10;
+
+int main() {
+  Singleton::Get().Hello();
+}
